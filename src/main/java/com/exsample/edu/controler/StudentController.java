023695365587af378a35cf56de633c65403978ff
meowmeuo <158165384+meowmeuo@users.noreply.github.com>
2025-03-22 15:39:54 +0300

    add getById

--- src/main/java/com/exsample/edu/controler/StudentController.java
+++ src/main/java/com/exsample/edu/controler/StudentController.java
@@ -1,10 +1,7 @@
 package com.exsample.edu.controler;
 
 import com.exsample.edu.model.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,5 +23,16 @@ public class StudentController {
         studentList.add(savedStudent);
         return savedStudent.getId();
     }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentByI(@PathVariable long id) {
+
+        for (Student student : studentList) {
+            if (student.getId() == id) return student;
+        }
+        
+        return null;
+    }
+
 
 }
